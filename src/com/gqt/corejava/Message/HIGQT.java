4ebe750e82541a8f60fd2190eb6e1620b2332be9
V Vinay Kumar <V Vinay Kumar@LAPTOP-KQMIE4P9>
2025-07-29 06:50:00 +0530

    loaded all programs

---
+++ src/com/gqt/corejava/Message/HIGQT.java
@@ -0,0 +1,11 @@
+package com.gqt.corejava.Message;
+
+public class HIGQT{
+    public static void main(String[] args) {
+        String message = "Hi GQT";
+        for (int i = 0; i < message.length(); i++) {
+            System.out.print(message.charAt(i) + " ");
+        }
+    }
+}
+
